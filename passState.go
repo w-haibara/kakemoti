5e56ef40bfb48199093a4078270ca8d312135824
w-haibara <hwhaibarawataru@gmail.com>
2021-09-25 20:36:32 +0900

    implement transition of pass state

--- passState.go
+++ passState.go
@@ -1,10 +1,29 @@
 package main
 
-import ()
+import (
+	"bytes"
+	"strings"
+)
 
 type PassState struct {
 	CommonState
 	Result     string `json:"Result"`
 	ResultPath string `json:"ResultPath"`
 	Parameters string `json:"Parameters"`
+}
+
+func (s PassState) Transition(r, w *bytes.Buffer) (next string, err error) {
+	if _, err := r.WriteTo(w); err != nil {
+		return "", err
+	}
+
+	if s.End {
+		return "", EndStateMachine
+	}
+
+	if strings.TrimSpace(s.Next) == "" {
+		return "", NextStateIsBrank
+	}
+
+	return s.Next, nil
 }
