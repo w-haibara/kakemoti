f887d3810b8b6a66af21d0e6b015d4a50303f612
w-haibara <hwhaibarawataru@gmail.com>
2021-11-14 02:31:00 +0900

    refactor

--- cmd/startExecution.go
+++ cmd/startExecution.go
@@ -10,20 +10,12 @@ import (
 
 func NewStartExecutionCmd() *cobra.Command {
 	o := new(statemachine.Options)
+	l := statemachine.NewLogger()
 
 	cmd := &cobra.Command{
 		Use:   "start-execution",
 		Short: "Starts a statemachine execution",
-		Run: func(cmd *cobra.Command, args []string) {
-			l := statemachine.NewLogger()
-			close := statemachine.SetLogWriter(l)
-			defer close()
-
-			ctx := context.Background()
-			if _, err := statemachine.Start(ctx, l, o); err != nil {
-				logrus.Fatal(err)
-			}
-		},
+		Run:   executionFn(o, l),
 	}
 
 	cmd.Flags().StringVar(&o.Input, "input", "", "path of a input json file")
@@ -31,4 +23,16 @@ func NewStartExecutionCmd() *cobra.Command {
 	cmd.Flags().Int64Var(&o.Timeout, "timeout", 0, "timeout of a statemachine")
 
 	return cmd
+}
+
+func executionFn(opt *statemachine.Options, logger *logrus.Entry) func(cmd *cobra.Command, args []string) {
+	return func(cmd *cobra.Command, args []string) {
+		close := statemachine.SetLogWriter(logger)
+		defer close()
+
+		ctx := context.Background()
+		if _, err := statemachine.Start(ctx, logger, opt); err != nil {
+			logrus.Fatal(err)
+		}
+	}
 }
