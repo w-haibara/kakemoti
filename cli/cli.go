fd310e198246c8e88ca83d5772eb65fd628e2b73
w-haibara <hwhaibarawataru@gmail.com>
2021-11-14 04:02:16 +0900

    refactor

--- cli/cli.go
+++ cli/cli.go
@@ -3,22 +3,35 @@ package cli
 import (
 	"bytes"
 	"context"
+	"io"
 	"os"
+	"path/filepath"
 	"strings"
+	"time"
 
 	"karage/log"
 	"karage/statemachine"
 )
 
 type Options struct {
+	Logfile string
 	Input   string
 	ASL     string
 	Timeout int64
 }
 
 func StartExecution(ctx context.Context, opt Options) ([]byte, error) {
+	if strings.TrimSpace(opt.Logfile) == "" {
+		opt.Logfile = "logs"
+	}
+
 	logger := log.NewLogger()
-	logger.SetWriter()
+	close := setLogOutput(logger, opt.Logfile)
+	defer func() {
+		if err := close(); err != nil {
+			panic(err.Error())
+		}
+	}()
 
 	if strings.TrimSpace(opt.Input) == "" {
 		logger.Fatalln("input option value is empty")
@@ -49,6 +62,26 @@ func StartExecution(ctx context.Context, opt Options) ([]byte, error) {
 	}()
 
 	return statemachine.Start(ctx, asl, input, opt.Timeout, logger)
+}
+
+func setLogOutput(l *log.Logger, path string) (close func() error) {
+	if _, err := os.Stat(path); err != nil {
+		if err := os.Mkdir(path, os.ModePerm); err != nil {
+			l.Fatal(err)
+		}
+	}
+
+	f, err := os.Create(filepath.Join(path, time.Now().Format("2006010215040507")+".log"))
+	if err != nil {
+		l.Fatal(err)
+	}
+
+	w := io.MultiWriter(os.Stderr, f)
+	l.Logger.SetOutput(w)
+
+	return func() error {
+		return f.Close()
+	}
 }
 
 func readFile(path string) (*os.File, *bytes.Buffer, error) {
