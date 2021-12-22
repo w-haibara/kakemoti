a08fda82c00f93462657b906f6df34053c5048df
w-haibara <hwhaibarawataru@gmail.com>
2021-12-22 23:51:33 +0900

    fix in/out of TaskState

--- task/task.go
+++ task/task.go
@@ -2,7 +2,6 @@ package task
 
 import (
 	"context"
-	"encoding/json"
 	"fmt"
 
 	"github.com/w-haibara/kuirejo/task/fn"
@@ -29,53 +28,30 @@ func Register(name string, fn Fn) {
 }
 
 func Do(ctx context.Context, resouceType, resoucePath string, input interface{}) (interface{}, error) {
-	fn, ok := fnMap[resouceType]
+	f, ok := fnMap[resouceType]
 	if !ok {
 		return nil, fmt.Errorf("invalid resouce type: %s", resouceType)
 	}
 
-	in, err := unmarshal(input)
-	if err != nil {
-		return nil, fmt.Errorf("unmarshal() failed: %v", err)
+	inMap, ok := input.(map[string]interface{})
+	if !ok {
+		return nil, fmt.Errorf("can not cast 'input' to map[string]fn.Obj")
 	}
 
-	out, err := fn(ctx, resoucePath, in)
+	in, ok := inMap[resouceType]
+	if !ok {
+		return nil, fmt.Errorf("'inObj' not contains the key: %s", resouceType)
+	}
+
+	obj, ok := in.(map[string]interface{})
+	if !ok {
+		return nil, fmt.Errorf("can not cast 'in' to fn.Obj")
+	}
+
+	out, err := f(ctx, resoucePath, obj)
 	if err != nil {
 		return nil, fmt.Errorf("fn() failed: %v", err)
 	}
 
-	output, err := marshal(out)
-	if err != nil {
-		return nil, fmt.Errorf("marshal() failed: %v", err)
-	}
-
-	return output, nil
-}
-
-func unmarshal(node interface{}) (fn.Obj, error) {
-	b, err := json.Marshal(node)
-	if err != nil {
-		return nil, err
-	}
-
-	var in fn.Obj
-	if err := json.Unmarshal(b, &in); err != nil {
-		return nil, err
-	}
-
-	return in, nil
-}
-
-func marshal(obj fn.Obj) (interface{}, error) {
-	b, err := json.Marshal(obj)
-	if err != nil {
-		return nil, err
-	}
-
-	var node interface{}
-	if err := json.Unmarshal(b, &node); err != nil {
-		return nil, err
-	}
-
-	return node, nil
+	return out, nil
 }
