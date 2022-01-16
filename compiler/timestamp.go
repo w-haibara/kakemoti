fa77c11e2bdb159334957d461cb995203b995841
w-haibara <hwhaibarawataru@gmail.com>
2022-01-16 23:32:41 +0900

    add Timestamp

---
+++ compiler/timestamp.go
@@ -0,0 +1,16 @@
+package compiler
+
+import "time"
+
+type Timestamp time.Time
+
+var TimestampFormat = "2006-01-02T15:04:05Z"
+
+func NewTimestamp(str string) (Timestamp, error) {
+	t, err := time.ParseInLocation(TimestampFormat, str, time.Now().Location())
+	if err != nil {
+		return Timestamp{}, err
+	}
+
+	return Timestamp(t), nil
+}
