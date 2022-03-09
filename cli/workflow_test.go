3d799e3d601ffb9658cffdefdb612b7a72fdc16a
w-haibara <hwhaibarawataru@gmail.com>
2022-03-09 21:29:52 +0900

    export test case

--- cli/workflow_test.go
+++ cli/workflow_test.go
@@ -10,43 +10,44 @@ import (
 	"github.com/w-haibara/kakemoti/compiler"
 )
 
-func TestExecWorkflowOnce_AND_RegisterWorkflow_ExecWorkflow(t *testing.T) {
-	type tcase struct {
-		name, asl, inputFile, wantFile string
-	}
-	tests := []tcase{
-		{"pass", "pass", "_workflow/inputs/input1.json", "_workflow/outputs/output1.json"},
-		{"pass(result)", "pass_result", "_workflow/inputs/input1.json", "_workflow/outputs/output5.json"},
-		{"pass(chain)", "pass_chain", "_workflow/inputs/input1.json", "_workflow/outputs/output1.json"},
-		{"pass(parameters)", "pass_parameters", "_workflow/inputs/input1.json", "_workflow/outputs/output11.json"},
-		{"pass(intrinsic)", "pass_intrinsic", "_workflow/inputs/input4.json", "_workflow/outputs/output12.json"},
-		{"pass(ctxobj)", "pass_ctxobj", "_workflow/inputs/input1.json", "_workflow/outputs/output10.json"},
-		{"wait", "wait", "_workflow/inputs/input1.json", "_workflow/outputs/output1.json"},
-		{"succeed", "succeed", "_workflow/inputs/input1.json", "_workflow/outputs/output1.json"},
-		{"fail", "fail", "_workflow/inputs/input1.json", "_workflow/outputs/output1.json"},
-		{"choice", "choice", "_workflow/inputs/input2.json", "_workflow/outputs/output2.json"},
-		{"choice(fallback)", "choice_fallback", "_workflow/inputs/input2.json", "_workflow/outputs/output7.json"},
-		{"choice(boolean expr)", "choice_bool", "_workflow/inputs/input5.json", "_workflow/outputs/output8.json"},
-		{"choice(data test expr)", "choice_data_test", "_workflow/inputs/input6.json", "_workflow/outputs/output8.json"},
-		{"parallel", "parallel", "_workflow/inputs/input2.json", "_workflow/outputs/output3.json"},
-		{"task", "task", "_workflow/inputs/input1.json", "_workflow/outputs/output4.json"},
-		{"task(filter)", "task_filter", "_workflow/inputs/input3.json", "_workflow/outputs/output9.json"},
-		{"task(catch)", "task_catch", "_workflow/inputs/input1.json", "_workflow/inputs/input1.json"},
-		{"task(retry)", "task_retry", "_workflow/inputs/input1.json", "_workflow/outputs/output8.json"},
-		{"task(ctxobj)", "task_ctxobj", "_workflow/inputs/input1.json", "_workflow/outputs/output10.json"},
-		{"map", "map", "_workflow/inputs/input7.json", "_workflow/outputs/output13.json"},
-		{"map(concurrency)", "map_concurrency", "_workflow/inputs/input7.json", "_workflow/outputs/output13.json"},
-		{"map(ctxobj)", "map_ctxobj", "_workflow/inputs/input8.json", "_workflow/outputs/output14.json"},
-		{"map(ctxobj2)", "map_ctxobj2", "_workflow/inputs/input8.json", "_workflow/outputs/output15.json"},
-	}
+type WorkflowExecTestCase struct {
+	name, asl, inputFile, wantFile string
+}
 
+var WorkflowExecTests = []WorkflowExecTestCase{
+	{"pass", "pass", "_workflow/inputs/input1.json", "_workflow/outputs/output1.json"},
+	{"pass(result)", "pass_result", "_workflow/inputs/input1.json", "_workflow/outputs/output5.json"},
+	{"pass(chain)", "pass_chain", "_workflow/inputs/input1.json", "_workflow/outputs/output1.json"},
+	{"pass(parameters)", "pass_parameters", "_workflow/inputs/input1.json", "_workflow/outputs/output11.json"},
+	{"pass(intrinsic)", "pass_intrinsic", "_workflow/inputs/input4.json", "_workflow/outputs/output12.json"},
+	{"pass(ctxobj)", "pass_ctxobj", "_workflow/inputs/input1.json", "_workflow/outputs/output10.json"},
+	{"wait", "wait", "_workflow/inputs/input1.json", "_workflow/outputs/output1.json"},
+	{"succeed", "succeed", "_workflow/inputs/input1.json", "_workflow/outputs/output1.json"},
+	{"fail", "fail", "_workflow/inputs/input1.json", "_workflow/outputs/output1.json"},
+	{"choice", "choice", "_workflow/inputs/input2.json", "_workflow/outputs/output2.json"},
+	{"choice(fallback)", "choice_fallback", "_workflow/inputs/input2.json", "_workflow/outputs/output7.json"},
+	{"choice(boolean expr)", "choice_bool", "_workflow/inputs/input5.json", "_workflow/outputs/output8.json"},
+	{"choice(data test expr)", "choice_data_test", "_workflow/inputs/input6.json", "_workflow/outputs/output8.json"},
+	{"parallel", "parallel", "_workflow/inputs/input2.json", "_workflow/outputs/output3.json"},
+	{"task", "task", "_workflow/inputs/input1.json", "_workflow/outputs/output4.json"},
+	{"task(filter)", "task_filter", "_workflow/inputs/input3.json", "_workflow/outputs/output9.json"},
+	{"task(catch)", "task_catch", "_workflow/inputs/input1.json", "_workflow/inputs/input1.json"},
+	{"task(retry)", "task_retry", "_workflow/inputs/input1.json", "_workflow/outputs/output8.json"},
+	{"task(ctxobj)", "task_ctxobj", "_workflow/inputs/input1.json", "_workflow/outputs/output10.json"},
+	{"map", "map", "_workflow/inputs/input7.json", "_workflow/outputs/output13.json"},
+	{"map(concurrency)", "map_concurrency", "_workflow/inputs/input7.json", "_workflow/outputs/output13.json"},
+	{"map(ctxobj)", "map_ctxobj", "_workflow/inputs/input8.json", "_workflow/outputs/output14.json"},
+	{"map(ctxobj2)", "map_ctxobj2", "_workflow/inputs/input8.json", "_workflow/outputs/output15.json"},
+}
+
+func TestExecWorkflowOnce_AND_RegisterWorkflow_ExecWorkflow(t *testing.T) {
 	if err := os.Chdir("../"); err != nil {
 		t.Error(`os.Chdir("../"):`, err)
 		return
 	}
 
-	type fn func(ctx context.Context, coj *compiler.CtxObj, tt tcase) ([]byte, error)
-	runTests := func(prefix string, f fn, tests []tcase) {
+	type fn func(ctx context.Context, coj *compiler.CtxObj, tt WorkflowExecTestCase) ([]byte, error)
+	runTests := func(prefix string, f fn, tests []WorkflowExecTestCase) {
 		for _, tt := range tests {
 			t.Run(prefix+"_"+tt.name, func(t *testing.T) {
 				ctx := context.Background()
@@ -87,7 +88,7 @@ func TestExecWorkflowOnce_AND_RegisterWorkflow_ExecWorkflow(t *testing.T) {
 		}
 	}
 
-	f1 := func(ctx context.Context, coj *compiler.CtxObj, tt tcase) ([]byte, error) {
+	f1 := func(ctx context.Context, coj *compiler.CtxObj, tt WorkflowExecTestCase) ([]byte, error) {
 		opt := ExecWorkflowOneceOpt{
 			RegisterWorkflowOpt: &RegisterWorkflowOpt{
 				ASL: "_workflow/asl/" + tt.asl + ".asl.json",
@@ -99,9 +100,9 @@ func TestExecWorkflowOnce_AND_RegisterWorkflow_ExecWorkflow(t *testing.T) {
 		}
 		return opt.ExecWorkflowOnce(ctx, coj, "", tt.name)
 	}
-	runTests("ExecWorkflowOnce", f1, tests)
+	runTests("ExecWorkflowOnce", f1, WorkflowExecTests)
 
-	f2 := func(ctx context.Context, coj *compiler.CtxObj, tt tcase) ([]byte, error) {
+	f2 := func(ctx context.Context, coj *compiler.CtxObj, tt WorkflowExecTestCase) ([]byte, error) {
 		o1 := RegisterWorkflowOpt{
 			ASL:          "_workflow/asl/" + tt.asl + ".asl.json",
 			WorkflowName: tt.name,
@@ -123,5 +124,5 @@ func TestExecWorkflowOnce_AND_RegisterWorkflow_ExecWorkflow(t *testing.T) {
 
 		return result, nil
 	}
-	runTests("RegisterWorkflow_ExecWorkflow", f2, tests)
+	runTests("RegisterWorkflow_ExecWorkflow", f2, WorkflowExecTests)
 }
