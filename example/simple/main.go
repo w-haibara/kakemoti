393cbee8487fda209d1a961b87b772072628a3d6
w-haibara <hwhaibarawataru@gmail.com>
2022-06-28 01:16:38 +0900

    add simple example

---
+++ example/simple/main.go
@@ -0,0 +1,48 @@
+package main
+
+import (
+	"bytes"
+	"context"
+	"fmt"
+	"log"
+
+	"github.com/w-haibara/kakemoti/compiler"
+	"github.com/w-haibara/kakemoti/worker"
+)
+
+func main() {
+	asl := `
+	{
+		"StartAt": "Pass State",
+		"States": {
+		    "Pass State": {
+		        "Type": "Pass",
+			    "End": true
+            }
+		},
+		"TimeoutSeconds": 0
+	}
+	`
+	input := `
+	{
+		"args": ["arg0", "arg1", "arg2"]
+	}	  
+	`
+
+	ctx := context.TODO()
+
+	fmt.Println("========= compiler.Compile =========")
+	fmt.Println("asl:", asl)
+	workflow, err := compiler.Compile(ctx, bytes.NewBufferString(asl))
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	fmt.Println("========= workflow.Exec =========")
+	fmt.Println("input:", input)
+	res, err := worker.Exec(ctx, nil, *workflow, bytes.NewBufferString(input))
+	if err != nil {
+		panic(err.Error())
+	}
+	fmt.Println("result:", string(res))
+}
